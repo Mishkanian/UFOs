daf9c39eea7c48ee045e8233d277dd76a8245e5d
Michael Mishkanian <77469811+Mishkanian@users.noreply.github.com>
2021-04-01 13:26:09 -0700

    Creating click function

--- static/js/app.js
+++ static/js/app.js
@@ -7,7 +7,6 @@ var tbody = d3.select("tbody");
 function buildTable(data) {
     // Clear out any existing data
     tbody.html("");
-  
     // Loop through each object in the data and append a row and cells for each value in the row
     data.forEach((dataRow) => {
         // Append a row to the table body
@@ -20,3 +19,23 @@ function buildTable(data) {
         );
     });
 }
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // Check to see if a date was entered and filter using that date.
+    if (date) {
+        // Apply `filter` to the table data to only keep the rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // Rebuild the table using the filtered data
+    // NOTE: If no date was entered, then filteredData will just be the original tableData.
+    buildTable(filteredData);
+  };
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
